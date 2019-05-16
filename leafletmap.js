a8fd918114dc06596c23b396d87788b289556b37
Sebastian Hancock <sebastian.hancock@uky.edu>
2019-05-16 13:18:17 +0200

    update

--- leafletmap.js
+++ leafletmap.js
@@ -1,6 +1,6 @@
 //initializes map and adds it to div id 'leaflet-map'
 var map = L.map('leaflet-map', {
-    center: [20.701, -157.465], //starting lat, lng
+    center: [20.7047, -157.6813], //starting lat, lng
     zoom: 7 //starting zoom
 });
 
@@ -15,41 +15,57 @@ layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{
 
 
 //add volcano icon
-var myIcon = L.icon({
+var volcanoIcon = L.icon({
     iconUrl: 'icons/volcano.png',
     iconSize: [30, 30],
-    iconAnchor: [20, 21]
+    iconAnchor: [20, 22]
+   
+})
+//add airpot icon
+var airportIcon = L.icon({
+    iconUrl: 'icons/airport-15.svg',
+    iconSize: [30, 30],
+    iconAnchor: [20, 22]
+})
+//add swimmming icon
+var swimIcon = L.icon({
+    iconUrl: 'icons/swimming.svg',
+    iconSize: [30, 30],
+    iconAnchor: [20, 22]
    
 })
 
 
-//adds marker to map and binds a popup on click
-var marker = L.marker([19.828, -155.470], {icon: myIcon}).addTo(map);
-marker.bindPopup('Mauna Kea');
+//adds markers to map and binds a popup on click
+L.marker([19.828, -155.470], {icon: volcanoIcon}).addTo(map).bindPopup('Mauna Kea');
+L.marker([21.289, -157.896], {icon: airportIcon}).addTo(map).bindPopup('Honolulu Airport');
+L.marker([21.669, -159.554], {icon: swimIcon}).addTo(map).bindPopup('Poipu Beach');
 
+
+//adds circle
+var circle = L.circleMarker([21.89, -160.15], {
+    radius:  16,
+    weight: 1.5,
+    color: 'purple',
+    fillColor: 'purple',
+    opacity: 0.2,
+    fillOpacity: 0.2
+}).addTo(map);
+
+circle.bindPopup("Nihue Island");
+
+/*
 //add polygon to map
 var polygon = L.polygon([
     [20.697, -156.562],
     [20.489, -156.079],
     [20.093, -156.497],
     [20.339, -157.222]
-]).addTo(map);
+]).addTo(map).bindPopup('omg polygon').openPopup();
 
-//binds a popup to the polygon and opens it when the map loads
-polygon.bindPopup('omg polygon').openPopup();
+*/
 
-
-//adds circle
-var circle = L.circleMarker([22.069, -159.500], {
-    radius:  15,
-    weight: 1.5,
-    color: 'purple',
-    fillColor: 'purple',
-    opacity: 0.4
-}).addTo(map);
-
-circle.bindPopup("here's a circle!");
-
-
-//sets max bounds as current bounds, so you can't move away from hawaii
-map.setMaxBounds(map.getBounds());
+//to help me get lat lng of areas
+map.on('click', function(e){
+    console.log(e.latlng)
+})
